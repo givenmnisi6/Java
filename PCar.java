1e1fba1b59e5485bc9dbdb579e01df3d3aa38975
GIVEN MNISI <81802355+givenmnisi6@users.noreply.github.com>
2021-11-06 01:28:05 +0200

    Add files via upload
    I DO 1

---
+++ PCar.java
@@ -0,0 +1,45 @@
+import java.lang.Math; 
+public class PCar extends Vehicles {
+    
+    //PCar - petrol car
+    private double gasolinetank;
+    private double petrolprice;
+
+    public PCar() {
+        this("","GVA 148 GP",1,5,1.0,17.50);
+    }
+
+    public PCar(String name, String noplate, int speed, int time, double gasolinetank, double petrolprice) {
+        super(name, noplate, speed, time);
+        setGasolinetank(gasolinetank);
+        setPetrolprice(petrolprice);
+    }
+
+    public void setGasolinetank(double gasolinetank) {
+        this.gasolinetank = gasolinetank;
+    }
+
+    public void setPetrolprice(double petrolprice) {
+        this.petrolprice = petrolprice;
+    }
+
+    public double getGasolinetank() {
+        return gasolinetank;
+    }
+
+    public double getPetrolprice() {
+        return petrolprice;
+    }
+
+    public double calclulateDistance() {
+        return getSpeed()*getTime();
+    }
+
+    public double calculateConsumption() {
+        return Math.round((getGasolinetank()*100.0)/getSpeed());
+    }
+    
+    public String toString() {
+        return "\nName: " + getName() + "\nNumber plate: " + getNoplate() + "\nSpeed: " + getSpeed() + " km" + "\nTime travelled: " + getTime() + " hour(s)" + "\nGas tank: " + getGasolinetank() + " litres" + "\nPetrol price: R " + getPetrolprice() + "\nDistance travelled: " + calclulateDistance() + " km/h" + "\nFuel consumption: " + calculateConsumption() + " litres/100kms";
+    }
+}
---
+++ Vehicles.java
@@ -0,0 +1,58 @@
+public abstract class Vehicles {
+	
+	private String name;
+	private String noplate;
+	private int speed;
+	private int time;
+	
+	//derfault constructor
+	public Vehicles() {
+		this("","GVA148GP",1,5);
+	}
+	
+	//constructor with parameters
+	public Vehicles(String name, String noplate, int speed, int time) {
+		setName(name);
+		setNoplate(noplate);
+		setSpeed(speed);
+		setTime(time);
+	}
+
+	//setMethods - Mutator
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setNoplate(String noplate) {
+		this.noplate = noplate;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	//getMethods - Accessor
+	public String getName() {
+		return name;
+	}
+
+	public String getNoplate() {
+		return noplate;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public abstract double calclulateDistance();
+	//fuelConsumption & electricConsumption
+	public abstract double calculateConsumption();	
+}
